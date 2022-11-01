f33920ff1c364da0f8211fae791d4a0ab6fda4cc
dohyun97 <dohyun647@gmail.com>
2022-11-01 17:00:00 -0400

    array

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return{
             product: 'Socks',
             image:'./assets/images/socks_green.jpg' ,
-            inveontory: 100 
+            inveontory: 100 ,
+            details: ['50% cotton','30% wool', '20% polyster'],
+            variants:[
+                {id: 2345, color:'green'},
+                {id:2234, color:'blue'} 
+            ]
         }
     }
 })
